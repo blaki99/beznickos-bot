0d43424ceb5d74e5608c0c5a92d01911f8531a99
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-23 15:57:56 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,15 +4,15 @@ const fs = require("fs");
 module.exports.run = async (blaki, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Nie posiadasz uprawnień.");
-  if(!args[0] || args[0 == "help"]) return message.reply("Użycie: -prefix <nowy prefix>");
+  if(!args[0] || args[0 == "help"]) return message.reply("Użycie: $prefix <nowy prefix>");
 
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  let prefixes = JSON.parse(fs.readFileSync("../prefixes.json", "utf8"));
 
   prefixes[message.guild.id] = {
     prefixes: args[0]
   };
 
-  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+  fs.writeFile("../prefixes.json", JSON.stringify(prefixes), (err) => {
   });
 
   let sEmbed = new Discord.RichEmbed()
