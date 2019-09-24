0473e8e282695d5375f9ebf229fabf4384e2bdc3
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-09-24 13:46:29 +0200

    Update msg.js

--- commands/msg.js
+++ commands/msg.js
@@ -19,7 +19,7 @@ module.exports.run = async (blaki, message, args) => {
 ${msg} ${msg2}${ramka2}`)
     .addField("__**OD:**__", `${message.author}`)
     .setTimestamp(message.createdAt)
-    .setFooter("Nowa Informacja", `${config.avatar}`);
+    .setFooter("Nowa Informacja", "https://i.imgur.com/g10ycEV.png");
     message.channel.send(msgEmbed);
 }
 
