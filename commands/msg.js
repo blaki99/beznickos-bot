60c5f4def7dab6cc9ead38c0166cba4f16431511
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-09-24 13:44:40 +0200

    Update msg.js

--- commands/msg.js
+++ commands/msg.js
@@ -1,28 +1,26 @@
 const Discord = require("discord.js");
+const config = require("../blakiconfig.json");
 
 module.exports.run = async (blaki, message, args) => {
 
-    let Szefuncio = message.guild.roles.find("name", "DISCORD MOD 🔧");
+    let host = message.guild.roles.get("579614246149685258");
 
     let msg = (args[0]);
     let msg2 = args.slice(1).join(' ')
-    const ramka = "```yaml"
+    const ramka = "```"
     const ramka2 = "```"
-    const check = '579026088487813160'
 
-    if(!message.member.roles.has(Szefuncio.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
+    if(!message.member.roles.has(host.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
     if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **!msg <wiadomość>**_ ❌")
     message.delete();
     let msgEmbed = new Discord.RichEmbed()
-    .setColor("#FF0000")
-    .setDescription(`**KOMUNIKAT OD:** ${message.author}`)
-    .addField("__**INFORMACJA**__", `${ramka}
+    .setColor("#6B7074")
+    .addField("__**INFORMACJA:**__", `${ramka}
 ${msg} ${msg2}${ramka2}`)
+    .addField("__**OD:**__", `${message.author}`)
     .setTimestamp(message.createdAt)
-    .setFooter("Kliknij reakcje jeśli przeczytałeś", "https://i.imgur.com/mNBIfzO.png");
-    message.channel.send(msgEmbed).then(function (message) {
-        message.react(check)
-    })
+    .setFooter("Nowa Informacja", `${config.avatar}`);
+    message.channel.send(msgEmbed);
 }
 
 module.exports.help = {
